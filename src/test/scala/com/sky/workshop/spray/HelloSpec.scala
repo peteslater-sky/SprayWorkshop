3d32dca84543d6e9b1275e5d103ec5eb26d3c45e
Slater, Peter <peter.slater@sky.uk>
2016-05-08 19:23:26 +0100

    Fixed broken spec and added service

--- src/test/scala/com/sky/workshop/spray/HelloSpec.scala
+++ src/test/scala/com/sky/workshop/spray/HelloSpec.scala
@@ -3,23 +3,13 @@ package com.sky.workshop.spray
 import org.specs2.mutable.Specification
 import spray.testkit.Specs2RouteTest
 import spray.routing.HttpService
-// import com.sky.workshop.spray.Hello.routes
 
-class HelloSpec extends Specification with Specs2RouteTest with HttpService {
+class HelloSpec extends Specification with Specs2RouteTest with HttpService with HelloService {
   def actorRefFactory = system
-
-  val routes =
-    path("hello") {
-      get {
-        complete {
-          <h1>Say hello to spray</h1>
-        }
-      }
-    }
 
   "The Hello endpoint" should {
     "return a greeting" in {
-      Get("/hello") ~> routes ~> check {
+      Get("/hello") ~> route ~> check {
         responseAs[String] must contain("Say hello")
       }
     }
